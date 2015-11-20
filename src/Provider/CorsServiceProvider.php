ba0b9ba2500abb36543b05fd7643c7433ebccdbb
Benoît Burnichon <bburnichon@alchemy.fr>
2015-11-20 18:55:08 +0100

    Sort in opposite direction

--- src/Provider/CorsServiceProvider.php
+++ src/Provider/CorsServiceProvider.php
@@ -131,7 +131,7 @@ CONFIG_EOF;
             $providersByPriority[$priority][] = $service;
         }
 
-        ksort($providersByPriority);
+        krsort($providersByPriority);
 
         return $providersByPriority;
     }
